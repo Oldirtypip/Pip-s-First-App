af50910f1d33cadf689fa73bf17249a2ef1aad4d
Oldirtypip <BigPip@MB-pro.local>
2015-02-26 18:22:00 -0800

    Initial Commit

---
+++ Pip's First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pip's First App
+//
+//  Created by Ryan Nicholson on 2/26/15.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
